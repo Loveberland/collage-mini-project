0934fa9cafce817d918d9093577f99e271dbf7e8
Loveberland <lovekittipong12@gmail.com>
2025-12-18 00:25:30 +0700

    add unix version

--- src/calculate.s
+++ src/calculate.s
@@ -8,66 +8,70 @@
 .global power
 .global findAbs
 
-// %rcx is first argument, %rdx is second argument, %rax is return registor
+/*
+	In unix %rdi is first argument register,
+	%rsi is second argument register,
+	%rax is return register
+*/
 
 add:
-        movq %rcx, %rax      
-        addq %rdx, %rax      
+        movq %rdi, %rax
+        addq %rsi, %rax
         ret
 
 sub:
-        movq %rcx, %rax      
-        subq %rdx, %rax      
+        movq %rdi, %rax
+        subq %rsi, %rax
         ret
 
 mul:
-        movq %rcx, %rax      
-        imulq %rdx, %rax     
+        movq %rdi, %rax
+        imulq %rsi, %rax
         ret
 
 div:
-        movq %rdx, %r8       
-        movq %rcx, %rax      
-        cqo                  
-        idivq %r8            
+        movq %rsi, %r8
+        movq %rdi, %rax
+        cqo
+        idivq %r8
         ret
 
 mod:
-        movq %rdx, %r8
-        movq %rcx, %rax
+        movq %rsi, %r8
+        movq %rdi, %rax
         cqo
-        idivq %r8       # rax = a / b
-        movq %rdx, %rax # reminder will assign to %rdx after use idivq
+        idivq %r8       /* rax = a / b */
+        movq %rsi, %rax /* reminder will assign to %rdx after use idivq */
         ret
 
 findSerie:
-        movq %rcx, %rax
-        movq %rcx, %r8
-        addq $1, %r8    # n + 1
-        imulq %r8, %rax # n(n + 1)
+        movq %rdi, %rax
+        movq %rdi, %r8
+        addq $1, %r8    /* n + 1 */
+        imulq %r8, %rax /* n(n + 1) */
         movq $2, %r9
         cqo
-        idivq %r9       # Divide rax by 2
+        idivq %r9       /* Divide rax by 2 */
         ret
 
 power:
         movq $1, %rax
 
 powerLoop:
-        cmpq $0, %rdx   # Check if EXP is 0
-        jle powerEnd    # If EXP <= 0, jump to end
+        cmpq $0, %rsi   /* Check if EXP is 0 */
+        jle powerEnd    /* If EXP <= 0, jump to end */
 
-        imulq %rcx, %rax
-        decq %rdx       # %rdx--
+        imulq %rdi, %rax
+        decq %rsi       /* %rdx-- */
         jmp powerLoop
 
 powerEnd:
         ret
 
 findAbs:
-        movslq %ecx, %rax
-        cmpq $0, %rax   # Compare %rax with 0
-        jge findAbsEnd  # If %rax greater than 0, end
+        movslq %edi, %rax
+        cmpq $0, %rax   /* Compare %rax with 0 */
+        jge findAbsEnd  /* If %rax greater than 0, end */
         negq %rax
 
 findAbsEnd:
